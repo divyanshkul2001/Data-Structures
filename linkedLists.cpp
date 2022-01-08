5f19b32750ef5fcf7245a54f202e5259c1e7f378
Divyansh Kulshreshtha <81429491+divyanshkul2001@users.noreply.github.com>
2022-01-08 12:35:52 +0530

    Update linkedLists.cpp

--- linkedLists.cpp
+++ linkedLists.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 class node{
+  
   public:
     int data;
     node* next;
@@ -14,6 +15,7 @@ class node{
 };
 
 void insertAtTail(node* &head, int value){
+    
     node* n = new node(value);
     
     //case 1 - empty linked list
@@ -38,12 +40,47 @@ void insertAtTail(node* &head, int value){
 }
 
 void insertAtHead(node* &head, int value){
+    
     node* n = new node(value);
     n -> next = head;
     head = n;
 }
 
+void deleteAtHead(node* &head){
+    
+    node* todelete = head;
+    head = head -> next;
+    delete todelete;
+}
+
+void deletion(node* &head, int value){
+    
+    //case 1 - when there is no linked list present
+    if(head == NULL){
+        return;
+    }
+    
+    //case 2 - when there is only one node present
+    if(head -> next == NULL){
+        deleteAtHead(head);
+    }
+    
+    //case 3 - when there is a linked list present with multiple nodes
+    node* temp = head;
+    
+    //loop until we reach the node before the deletion node(n-1th node)
+    while(temp -> next -> data != value){
+        temp = temp -> next;
+    }
+    
+    //store the node to be delete
+    node* todelete = temp -> next;
+    temp -> next = temp -> next -> next;
+    delete todelete;
+}
+
 void display(node* head){
+    
     node* temp = head;
     while(temp!=NULL)
     {
@@ -53,8 +90,8 @@ void display(node* head){
     cout<<"NULL"<<endl;
 }
 
-int main()
-{
+int main(){
+    
     /*node* head = NULL;
     insertAtTail(head, 1);
     insertAtTail(head, 2);
@@ -65,8 +102,14 @@ int main()
 
     insertAtTail(n,2);
     insertAtTail(n,3);
+    insertAtTail(n,5);
+    insertAtTail(n,6);
     display(n);
     insertAtHead(n,4);
+    display(n);
+    
+    deletion(n,6);
+    deleteAtHead(n);
     display(n);
     return 0;
 }
